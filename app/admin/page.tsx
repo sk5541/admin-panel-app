1c48e65fb65f0c6c25e7b914b3fe21e52d7a7f83
Skhaton23 <skhaton237@gmail.com>
2026-03-05 14:29:17 -0500

    added supabase connection

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,21 @@
+"use client"
+import { useEffect, useState } from "react"
+import supabase from "@/lib/supabaseClient"
+
 export default function AdminPage() {
+    const [userCount, setUserCount ] = useState(0)
+    useEffect(() => {
+        getStats()
+    }, [])
+
+    async function getStats() {
+        const { count } = await supabase
+            .from("profiles")
+            .select("*", {count: "exact", head: true})
+
+        setUserCount(count || 0)
+    }
+
     return (
         <div>
             <h1>Admin Dashboard</h1>
