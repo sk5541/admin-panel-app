32bdc79e9091e091946b790c7583daa40a2e2123
Skhaton23 <skhaton237@gmail.com>
2026-03-07 16:49:41 -0500

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
-import { redirect } from "next/navigation";
-export default function Home() {
-  redirect("/admin");
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home(){
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/admin");
+  }, [router]);
+  return <p>Loading admin panel...</p>;
 }
